dfe784ea36709d7750cadeec70853d02ab5e22ed
YeloHawPi <w.yelohawpi@gmail.com>
2017-10-24 20:17:42 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,28 +13,6 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 	bot.sendMessage(chatId, resp);
 });
 
-// напомни
-var notes = [];
-
-bot.onText(/\/напомни (.+) в (.+)/, (msg, match) => {
-    var chatId = msg.chat.id;
-    var text = match[1];
-    var time = match[2];
-
-    notes.push( { 'uid':chatId, 'time':time, 'text':text } );
-
-	bot.sendMessage(chatId, 'Хорошо я обязательно вам напомню! Если не сдохну :)');
-});
-
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++){
-        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-        if ( notes[i]['time'] == curDate ) {
-            bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы сейчас должны: '+ notes[i]['text']);
-            notes.splice(i,1);
-        }
-    }
-},1000); 
 
 // img
 bot.onText(/\/img/, (msg) => {
@@ -44,28 +22,12 @@ bot.onText(/\/img/, (msg) => {
 	bot.sendPhoto(chatId, url);
 });
 
-// mem
-bot.onText(/\/mem/, (msg) => {
+// command
+bot.onText(/\/command/, (msg) => {
 	const chatId = msg.chat.id;
-	var memUrl = 'Mems/' + (Math.floor(Math.random() * (20 - 1)) + 1) + '.jpg';
+	var Url = 'Folder/' + (Math.floor(Math.random() * (max - min)) + min) + '.jpg';
 
-	bot.sendPhoto(chatId, memUrl);
-});
-
-// loli
-bot.onText(/\/loli/, (msg) => {
-	const chatId = msg.chat.id;
-	var loliUrl = 'Loli/' + (Math.floor(Math.random() * (11 - 1)) + 1) + '.jpg';
-
-	bot.sendPhoto(chatId, loliUrl);
-});
-
-// шлюха
-bot.onText(/\/шлюха/, (msg) => {
-	const chatId = msg.chat.id;
-	var whoreUrl = 'Whore/' + (Math.floor(Math.random() * (36 - 1)) + 1) + '.jpg';
-
-	bot.sendPhoto(chatId, whoreUrl);
+	bot.sendPhoto(chatId, Url);
 });
 
 // ----------------------------
